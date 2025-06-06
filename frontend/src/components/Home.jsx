efd40cc1646082c7f4192b70eb6e6ebae6369d75
jfzara <zarajeanfabrice@gmail.com>
2025-06-05 20:15:46 -0400

    Création des composants pour la page d'accueil

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+import Header from './Header';
+import Conteneur from './Conteneur';
+import Footer from './Footer';
+
+const Home = () =>{
+
+return(
+<>
+<Header/>
+<Conteneur/>
+<Footer/>
+</>
+)
+
+}
+export default Home;
